c6edfa8eaa4af78680ddf753a3cd7d14bdb0b003
alena-vovkohon <alena.vovkogon@gmail.com>
2022-05-22 18:51:16 +0300

    homework#13

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 // Перший спосіб через querySelectorAll
-// export const paintCards = () => {
-//     let elemLi = document.querySelectorAll('li:nth-of-type(2n-1)');
-//     for (let li of elemLi) {
-//         li.style.backgroundColor = "red";
-//     }
-// };
+export const paintCards = () => {
+    let elemLi = document.querySelectorAll('li:nth-of-type(2n-1)');
+    for (let li of elemLi) {
+        li.style.backgroundColor = "red";
+    }
+};
 
 // Другий спосіб через children
-export const paintCards = () => {
-     Array.from(document.querySelector('ul').children).filter((item, index) => {
-        if (index % 2 === 0) {
-            return item.style.backgroundColor = "red";
-        }      
-     })
-};
+// export const paintCards = () => {
+//      Array.from(document.querySelector('ul').children).filter((item, index) => {
+//         if (index % 2 === 0) {
+//             return item.style.backgroundColor = "red";
+//         }      
+//      })
+// };
 
 export const findElement = () => {
     let elemLi = document.querySelectorAll('li');
