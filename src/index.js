af974e51107d42a20dbef9ad618c312e694388a4
alena-vovkohon <alena.vovkogon@gmail.com>
2022-05-22 18:09:37 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+    let elemLi = document.querySelectorAll('li:nth-of-type(2n-1)');
+    for (let li of elemLi) {
+        li.style.backgroundColor = "red";  
+    }
+};
 
-export const findElement = () => {};
+export const findElement = () => {
+    let elemLi = document.querySelectorAll('li');
+
+    for (let li of elemLi) {
+        if (li.matches('.likedItem')) {
+            li.style.backgroundColor = "blue"; 
+        }
+    }
+
+};
